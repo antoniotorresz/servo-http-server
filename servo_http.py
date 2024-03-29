cd78a65e23cfccb4849aba41921f686f20962672
antoniotorresz <antoniiotorresz1@gmail.com>
2024-03-28 19:47:44 -0600

    added cors orirgin

--- servo_http.py
+++ servo_http.py
@@ -1,12 +1,41 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
 from datetime import datetime
 from sqlalchemy import event
-import servo_hardware
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
+
+DOCUMENTATION_HTML = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Servo HTTP Server</title>
+</head>
+<body>
+    <h1>Servo HTTP Server API Documentation</h1>
+
+    <h2>/ (GET)</h2>
+    <p>Returns the home page with information about the actions in the database.</p>
+    <p>Returns a dictionary containing the total number of actions, details of the last action, details of all actions, and the current server time.</p>
+
+    <h2>/switch-action (POST)</h2>
+    <p>Switches the current action by creating a new Action object with the opposite 'is_activated' value.</p>
+    <p>Returns a dictionary containing a message indicating the action switch and the details of the new action.</p>
+
+    <h2>/init (GET)</h2>
+    <p>Initializes the action database by creating a new Action object with 'is_activated' set to False.</p>
+    <p>Returns a dictionary containing a message indicating the action initialization and the details of the new action.</p>
+
+    <h2>/log (GET)</h2>
+    <p>Returns a dictionary containing the total number of actions, details of the last action, details of all actions, and the current server time.</p>
+</body>
+</html>
+"""
+
 
 class Action(db.Model):
     """
@@ -54,10 +83,13 @@ def on_is_activated_change(target, value, oldvalue, initiator):
         It prints a message indicating whether the servo is being activated or deactivated.
     """
     if value:
-        servo_hardware.move_servo()
+        pass
+        #servo_hardware.move_servo()
     else:
-        servo_hardware.stop_servo()
+        pass
+        #servo_hardware.stop_servo()
 
+@cross_origin()
 @app.route('/switch-action', methods=['POST'])
 def switch_action():
     """
@@ -75,6 +107,7 @@ def switch_action():
         "action": new_action.to_dict()
     }
 
+@cross_origin()
 @app.route('/init', methods=['GET'])
 def init_action_database():
     """
@@ -91,6 +124,25 @@ def init_action_database():
         "action": new_action.to_dict()
     }
 
+@cross_origin()
+@app.route('/log')
+def log():
+    """
+    Returns the home page with information about the actions in the database.
+
+    Returns:
+        dict: A dictionary containing the total number of actions, details of the last action, details of all actions,
+              and the current server time.
+    """
+    actions = [action.to_dict() for action in Action.query.all()]
+    return {
+        "total_actions": Action.query.count(),
+        "last_action": Action.query.order_by(Action.id.desc()).first().to_dict(),
+        "actions": actions[::-1],
+        "server_time": f"{datetime.now()}"
+    }
+
+@cross_origin()
 @app.route('/')
 def home():
     """
@@ -100,12 +152,8 @@ def home():
         dict: A dictionary containing the total number of actions, details of the last action, details of all actions,
               and the current server time.
     """
-    return {
-        "total_actions": Action.query.count(),
-        "last_action": Action.query.order_by(Action.id.desc()).first().to_dict(),
-        "actions": [action.to_dict() for action in Action.query.all()],
-        "server_time": f"{datetime.now()}"
-    }
+      # Update the template file name to 'home.html'
+    return DOCUMENTATION_HTML
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
